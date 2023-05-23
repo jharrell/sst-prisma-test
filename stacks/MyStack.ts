c2ef3cffdbc2585ac4c294ba6880b3357104e4dc
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2023-05-23 13:21:12 -0500

    copy schema.prisma into SST bundle

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -2,6 +2,11 @@ import { StackContext, Api } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
   const api = new Api(stack, "api", {
+    defaults: {
+      function: {
+        copyFiles: [{ from: 'packages/core/prisma/schema.prisma', to: 'packages/functions/src/schema.prisma' }],
+      },
+    },
     routes: {
       "GET /": "packages/functions/src/lambda.handler",
     },
